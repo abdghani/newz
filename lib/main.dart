2df623b22f375dbfca273266a2484bd8a7151e6c
abdullah ghani <abdullah.ghani3@gmail.com>
2021-07-18 10:56:47 +0530

    apk update

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyAppState extends State<MyApp> {
             return MaterialApp(
                 scaffoldMessengerKey: _messangerKey,
                 debugShowCheckedModeBanner: false,
-                title: 'Zaream',
+                title: 'Vantage',
                 routes: {
                   '/news': (context) => NewsContainer(),
                 },
